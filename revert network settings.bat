6b393fa70f964828162f863e370107125cc6f80a
Gabriel Solomon Holland <gabrielsolomonholland@gmail.com>
2023-07-31 10:42:35 -0400

    Batch  files for running scripts

---
+++ revert network settings.bat
@@ -0,0 +1,2 @@
+Set-ExecutionPolicy -ExecutionPolicy bypass
+powershell -NoProfile -windowstyle hidden -Command "Start-Process -Verb RunAs powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File .\RevertSettings.ps1 -_vLUF %_vLUF%'"
